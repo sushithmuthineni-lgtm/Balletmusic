5fb51d9dad7f9508dc838394008d4891aa15f23c
sushithmuthineni-lgtm <sushithmuthineni@gmail.com>
2026-09-07 13:04:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ dns.setDefaultResultOrder('ipv4first');
 process.env.FFMPEG_PATH = require('ffmpeg-static');
 
 const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
-const { Player } = require('discord-player');
+const { YoutubeExtractor, SoundCloudExtractor, SpotifyExtractor } = require('@discord-player/extractor');
+
 
 const TOKEN = process.env.DISCORD_TOKEN;
 const PREFIX = process.env.PREFIX || '!';
