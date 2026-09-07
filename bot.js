d64be568e6f67fd7355e03e621f4dd265a823204
sushithmuthineni-lgtm <sushithmuthineni@gmail.com>
2026-09-07 20:25:27 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,11 @@
 /*
 
-  ██████╗░████████╗██╗░░██╗           
-  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
-  ██████╔╝░░░██║░░░░╚███╔╝░          
-  ██╔══██╗░░░██║░░░░██╔██╗░          
-  ██║░░██║░░░██║░░░██╔╝╚██╗          
-  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-
-
-   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-   ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
+                
+       ██████╗   █████╗   ██╗     ██╗          ███████╗   ████████╗
+       ██╔══██╗ ██╔══██╗ ██║     ██║         ██╔════╝   ╚══██╔══╝
+       ██████╔╝ ███████║ ██║     ██║         █████╗         ██║   
+       ██╔══██╗ ██╔══██║ ██║     ██║         ██╔══╝         ██║   
+       ╚═════╝  ╚═╝  ╚═╝  ╚══════╝╚══════╝  ╚══════╝      ╚═╝   
 */
 
 const { Client, GatewayIntentBits, Partials } = require("discord.js");
@@ -129,16 +123,9 @@ printWatermark();
 
 /*
 
-  ██████╗░████████╗██╗░░██╗           
-  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
-  ██████╔╝░░░██║░░░░╚███╔╝░          
-  ██╔══██╗░░░██║░░░░██╔██╗░          
-  ██║░░██║░░░██║░░░██╔╝╚██╗          
-  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-
-
-   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-   ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
+      ██████╗   █████╗   ██╗     ██╗          ███████╗   ████████╗
+       ██╔══██╗ ██╔══██╗ ██║     ██║         ██╔════╝   ╚══██╔══╝
+       ██████╔╝ ███████║ ██║     ██║         █████╗         ██║   
+       ██╔══██╗ ██╔══██║ ██║     ██║         ██╔══╝         ██║   
+       ╚═════╝  ╚═╝  ╚═╝  ╚══════╝╚══════╝  ╚══════╝      ╚═╝   
 */
